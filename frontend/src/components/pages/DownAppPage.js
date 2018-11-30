690765e49d43b59d5167ce70056d8f93318cca7d
Hitaro1985 <tomasfans1985@gmail.com>
2018-12-01 02:31:41 +0800

    update down page

--- frontend/src/components/pages/DownAppPage.js
+++ frontend/src/components/pages/DownAppPage.js
@@ -6,6 +6,8 @@ var request = require('superagent');
 var QRCode = require('qrcode.react');
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 var platform = require('platform');
+var Axios = require('axios');
+var RNFS = require('react-native-fs');
 
 class DownAppPage extends Component {
     constructor(props) {
@@ -15,7 +17,9 @@ class DownAppPage extends Component {
             loadingmessage: 'please wait a moment...',
             error: '',
             applink: '',
-            appres: null
+            appres: null,
+            downloading: false,
+            progress: null,
         };
     }
 
@@ -44,7 +48,30 @@ class DownAppPage extends Component {
 
     handleClick(event) {
         event.preventDefault();
-        window.open(`${global.baseUrl}${this.state.applink}?platform=${platform.os.family}`, "_blank");
+        this.setState({ downloading: true});
+        const url = `${global.baseUrl}${this.state.applink}?platform=${platform.os.family}`;
+        if (platform.os.family == 'Android') {
+            const path = RNFS.DocumentDirectoryPath + '/' + this.props.match.params.applink + '.apk';
+            var download = RNFS.downloadFile({
+                fromUrl: url,
+                toFile: path,
+                progress: res => {
+                    this.setState({ progress: (res.bytesWritten / res.contentLength).toFixed(2) });
+                    console.log((res.bytesWritten / res.contentLength).toFixed(2));
+                },
+                progressDivider: 1
+            });
+            download.promise.then(result => {
+                if(result.statusCode == 200){
+                    NativeModules.InstallApk.install(filePath);
+                    this.setState({ progress: null });
+                    this.setState({ downloading: false });
+                } else {
+                    this.setState({ downloading: false });
+                }
+            });
+        }
+        // window.open(`${global.baseUrl}${this.state.applink}?platform=${platform.os.family}`, "_blank");
     }
 
     render() {
@@ -77,9 +104,15 @@ class DownAppPage extends Component {
                                         <QRCode value={"http://47.100.36.49:3000/"+ this.state.applink}/>
                                         </div>
                                         { (platform.os.family == 'Android' || platform.os.family == 'iOS') && (
+                                            (this.state.downloading) ? (
                                         <MuiThemeProvider>
                                             <RaisedButton label="Download" primary={true} style={style} onClick={(event) => this.handleClick(event)}/>
-                                        </MuiThemeProvider>
+                                        </MuiThemeProvider>) : (
+                                            <div>
+                                                Downloading.....
+                                                {this.state.progress}{' '}Bytes downloaded.
+                                            </div>
+                                        )
                                         )}
                                     </div>
                                     <h1 className="name">
